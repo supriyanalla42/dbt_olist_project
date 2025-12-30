18f84e89b2c10131ae8ab1e3c1cfa30ffb59d451
student <student@localhost.localdomain>
2025-12-30 17:38:32 +0530

    day11 make fct_orders incremental with merge

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,20 +1,60 @@
-select
-  order_id,
-  customer_id,
-  order_status,
-  order_purchase_ts,
-  order_approved_ts,
-  order_delivered_carrier_ts,
-  order_delivered_customer_ts,
-  order_estimated_delivery_date,
+{{
+  config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key='order_id',
+    on_schema_change='sync_all_columns'
+  )
+}}
 
-  order_item_row_count,
-  distinct_product_count,
-  items_total_value,
-  freight_total_value,
+WITH orders_enriched AS (
+    SELECT
+        order_id,
+        customer_id,
+        order_status,
+        order_purchase_ts,
+        order_approved_ts,
+        order_delivered_carrier_ts,
+        order_delivered_customer_ts,
+        order_estimated_delivery_date,
 
-  payment_total_value,
-  max_payment_installments,
-  payment_row_count,
-  payment_type_any
-from {{ ref('int_orders_enriched') }}
+        order_item_row_count,
+        distinct_product_count,
+        items_total_value,
+        freight_total_value,
+
+        payment_total_value,
+        max_payment_installments,
+        payment_row_count,
+        payment_type_any
+    FROM {{ ref('int_orders_enriched') }}
+
+    {% if is_incremental() %}
+    WHERE order_purchase_ts >= (
+        SELECT
+            DATEADD(day, -3, MAX(order_purchase_ts))
+        FROM {{ this }}
+    )
+    {% endif %}
+)
+
+SELECT
+    order_id,
+    customer_id,
+    order_status,
+    order_purchase_ts,
+    order_approved_ts,
+    order_delivered_carrier_ts,
+    order_delivered_customer_ts,
+    order_estimated_delivery_date,
+
+    order_item_row_count,
+    distinct_product_count,
+    items_total_value,
+    freight_total_value,
+
+    payment_total_value,
+    max_payment_installments,
+    payment_row_count,
+    payment_type_any
+FROM orders_enriched
